264e47dea7d401451cb37080432f26a84f898ac0
laminesunga <laminesunga@gmail.com>
2023-03-30 03:01:17 -0700

    add env

--- services/auth.js
+++ services/auth.js
@@ -3,11 +3,18 @@ var mysql = require('mysql');
  
 // create a connection variable with the required details
 var con = mysql.createConnection({
-  host: process.env.host,// ip address of server running mysql
-  user: process.env.user, // user name to your mysql database
-  password: process.env.password, // corresponding password
-  port: process.env.port,
-  database: process.env.database // use the specified database
+  // host: process.env.host,// ip address of server running mysql
+  // user: process.env.user, // user name to your mysql database
+  // password: process.env.password, // corresponding password
+  // port: process.env.port,
+  // database: process.env.database // use the specified database
+
+  host:"lam-rds.cnte09ntnetb.us-west-1.rds.amazonaws.com",
+  user:"admin",
+  password: "Lamine1997",
+  port:"3306",
+  database:"lamine"
+
 });
  
 // make to connection to the database.
